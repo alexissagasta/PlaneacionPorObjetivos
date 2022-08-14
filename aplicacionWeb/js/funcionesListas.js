f49cdd52e372890698ba9bdc7b1dfbfb7167abe8
luisiyi <luisiyi1997@gmail.com>
2022-08-14 13:29:08 -0700

    Cambios en página principal
    Las redirecciones están funcionando, el footer ya esta correcto, se liminaron los botones de ingresar y registrarse de la página principal, ya no muestra los objetivos repetidos ni las areas, ya muestra como tabla los planes de acción (solo es 1), se elimino el índice mostrado para trabajadores.

--- aplicacionWeb/js/funcionesListas.js
+++ aplicacionWeb/js/funcionesListas.js
@@ -1,7 +1,8 @@
 nombresAreasEnfoque = [];
 nombresObjetivos = [];
+nombresPlanesAcciones = [];
+//Obtención de los nombres o titulos de las areas de enfoque
 async function obtenerAreas() {
-    //
     const response = await fetch("/areasDeEnfoque", {
       method: 'GET',
       headers: {
@@ -11,15 +12,52 @@ async function obtenerAreas() {
     })
   
     const data = await response.json();
-    const lts = data;
-    lts.forEach(lt => {
-  
-      const nombre = lt.nombre;
-      const objetivos = lt.objetivos;
+    const opcionesAreas = data;
+
+    for(let i = 0; i < opcionesAreas.nombre; i++){
+      const nombre = opcionesAreas[i];
       nombresAreasEnfoque.push(nombre);
-      nombresObjetivos.push(objetivos);
-  
-    });
+    }
+    
+}
+//Obtención de los objetivos de las areas de enfoque
+async function obtenerObjetivos() {
+  const response = await fetch("/areasDeEnfoque", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  })
+
+  const data = await response.json();
+  const opcionesObjetivos = data;
+
+  for(let i = 0; i < opcionesObjetivos.objetivos; i++){
+    const objetivos = opcionesObjetivos[i];
+    nombresObjetivos.push(objetivos);
+  }
+
+}
+
+async function obtenerTituloPlanesAcciones() {
+  //
+  const response = await fetch("/titulosPlanesDeAccion", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  })
+
+  const data = await response.json();
+  const opcionesTituloPlanesAcciones = data;
+
+  for(let i = 0; i < opcionesTituloPlanesAcciones.length; i++){
+    const titulosPlanes = opcionesTituloPlanesAcciones[i];
+    nombresPlanesAcciones.push(titulosPlanes);
+  }
+
 }
 
 
@@ -43,8 +81,8 @@ async function crearTablaAreas(idAreas) {
     
   }
 
-  async function crearTablaObjetivos(idAreas) {
-    await obtenerAreas()
+  async function crearTablaObjetivos(idObjetivos) {
+    await obtenerObjetivos();
   
     // Se crea el formulario para seleccionar en elcargado del plan 
     //por ahora se establece un valor pre establecido ya que no se
@@ -59,10 +97,31 @@ async function crearTablaAreas(idAreas) {
    objetivosTodas += "</table>"
  
    //Se cambia el contenido de los elementos acciones, recursos, encargados e indicadores
-   document.getElementById(idAreas).innerHTML = objetivosTodas;
+   document.getElementById(idObjetivos).innerHTML = objetivosTodas;
+    
+  }
+
+  async function crearTablaPlanesAcciones(idPlanes) {
+    await obtenerTituloPlanesAcciones();
+  
+    // Se crea el formulario para seleccionar en elcargado del plan 
+    //por ahora se establece un valor pre establecido ya que no se
+    //manejan usuarios aun
+    
+  
+    // Tabla de contenido para llenar las acciones al momento de seleccionar un plan de acción
+   let planAccionTodas = "<table class=center style =  background-color:#FFFFFF ><tr class=cabecera style = contenteditable:false ><td></td></tr>";
+   for (let i = 0; i < nombresPlanesAcciones.length; i++) {
+    planAccionTodas += "<tr><td contenteditable=false style=text-align:center >" + nombresPlanesAcciones[i] + "</td></tr>";
+   }
+   planAccionTodas += "</table>"
+ 
+   //Se cambia el contenido de los elementos acciones, recursos, encargados e indicadores
+   document.getElementById(idPlanes).innerHTML = planAccionTodas;
     
   }
   
   this.onload = crearTablaAreas("tablaAreas");
   this.onload = crearTablaObjetivos("tablaObjetivos");
+  this.onload = crearTablaPlanesAcciones("tablaPlanesAcciones");
 
